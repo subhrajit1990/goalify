5cb3eb63db340ba1be90c9096c64b313fad3161c
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2025-09-03 09:33:06 +0000

    ch

--- goalify/src/components/TaskBoard.js
+++ goalify/src/components/TaskBoard.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import TaskInput from "./TaskInput";
 import StickyNote from "./StickyNote";
 import "../styles/TaskBoard.css";
 
-export default function TaskBoard({ tasks, onDelete, onComplete, onReschedule }) {
+export default function TaskBoard() {
+  const [tasks, setTasks] = useState([]);
+
+  const handleAddTask = (task) => {
+    setTasks((prev) => [
+      ...prev,
+      { ...task, fromPocket: true, x: 50, y: 50 }
+    ]);
+  };
+
   return (
     <div className="task-board">
-      {tasks.map((task) => (
-        <StickyNote
-          key={task.id}
-          task={task}
-          onDelete={onDelete}
-          onComplete={onComplete}
-          onReschedule={onReschedule}
-        />
-      ))}
+      <TaskInput onAdd={handleAddTask} />
+      <div id="notes-area" className="notes-area">
+        {tasks.map((t) => (
+          <StickyNote
+            key={t.id}
+            task={t}
+            onDelete={(id) => setTasks((p) => p.filter((tt) => tt.id !== id))}
+          />
+        ))}
+      </div>
     </div>
   );
 }
